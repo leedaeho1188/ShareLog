41e49493bb2c55af3db83cc1b9817d726a9cf58e
Daeho Lee <67696504+leedaeho1188@users.noreply.github.com>
2021-02-25 15:54:48 +0900

    1

--- templates/app.js
+++ templates/app.js
@@ -1,9 +1,9 @@
 const toggleBtn = document.querySelector('.navbar_toogleBtn')
 const menu = document.querySelector('.navbar_menu')
-const button = document.querySelector('.navbar_writeBtn')
+const writeBtn = document.querySelector('.navbar_writeBtn')
+
 
 toggleBtn.addEventListener('click', () => {
   menu.classList.toggle('active');
-  button.classList.toggle('active')
+  writeBtn.classList.toggle('active')
 })
-
